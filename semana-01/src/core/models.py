d052a663615348e7e2661a75befb43f6978583c5
Davish-pxl <guettavalcarcel@gmail.com>
2026-09-09 23:16:55 -0500

    Laboratorio 01 completado: Items registrados, vistas verificadas y dependencias guardadas

---
+++ semana-01/src/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Item(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Nombre")
+    description = models.TextField(blank=True, verbose_name="Descripción")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
+        ordering = ['-created_at']
