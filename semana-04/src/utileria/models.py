8905f6df8e84fa3a0eb2d2902db7901fdc99870c
Davish-pxl <guettavalcarcel@gmail.com>
2026-09-11 00:14:58 -0500

    Relacion 1:N entre Categoria y Producto con PROTECT

--- semana-04/src/utileria/models.py
+++ semana-04/src/utileria/models.py
@@ -8,8 +8,8 @@ class Proveedor(models.Model):
         return self.razon_social
 
 class Categoria(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=150)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.nombre
@@ -26,6 +26,13 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.PROTECT,
+        related_name='productos',
+        null=True, 
+        blank=True
+    )
 
     def __str__(self):
         return self.nombre
