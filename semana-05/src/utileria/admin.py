156612e076d5d0e9257b3dfef95ac20d018c1f4c
noemidelacruz <noemi.delacruz@tecsup.edu.pe>
2026-09-20 00:38:01 -0500

    Lab 05-ejercicio del 02 al 06

--- semana-05/src/utileria/admin.py
+++ semana-05/src/utileria/admin.py
@@ -4,58 +4,40 @@ from .models import (
     FichaTecnicaProducto, UbicacionAlmacen, Pedido, DetallePedido, ComprobantePago
 )
 
+admin.site.register(Categoria)
+admin.site.register(PerfilCliente)
+admin.site.register(UbicacionAlmacen)
+admin.site.register(DetallePedido)
+admin.site.register(ComprobantePago)
+
 
 class FichaTecnicaInline(admin.StackedInline):
     model = FichaTecnicaProducto
-    extra = 1
+    extra = 0
 
-class UbicacionAlmacenInline(admin.StackedInline):
-    model = UbicacionAlmacen
-    extra = 1
-
-class PerfilClienteInline(admin.StackedInline):
-    model = PerfilCliente
-    extra = 1
-
-class ComprobantePagoInline(admin.StackedInline):
-    model = ComprobantePago
-    extra = 1
-
-class DetallePedidoInline(admin.TabularInline):
-    model = DetallePedido
-    extra = 1
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'nombre', 'precio', 'stock', 'categoria')
-    search_fields = ('codigo', 'nombre')
+    search_fields = ('nombre', 'codigo')
     list_filter = ('categoria',)
-    inlines = [FichaTecnicaInline, UbicacionAlmacenInline]
+    inlines = [FichaTecnicaInline]
+
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'telefono') 
-    search_fields = ('nombre', 'telefono')
-    inlines = [PerfilClienteInline]
+    list_display = ('nombre', 'telefono')
+    search_fields = ('nombre',)
+
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
     list_display = ('id', 'cliente', 'proveedor', 'fecha', 'estado')
+    search_fields = ('cliente__nombre', 'estado')
     list_filter = ('estado', 'fecha')
-    inlines = [DetallePedidoInline, ComprobantePagoInline]
+
 
 @admin.register(Proveedor)
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ('ruc', 'razon_social')
-    search_fields = ('ruc', 'razon_social')
-
-@admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'descripcion')
-    search_fields = ('nombre',)
-
-admin.site.register(FichaTecnicaProducto)
-admin.site.register(UbicacionAlmacen)
-admin.site.register(PerfilCliente)
-admin.site.register(DetallePedido)
-admin.site.register(ComprobantePago)
+    list_display = ('razon_social', 'ruc')
+    search_fields = ('razon_social', 'ruc')
